a02cbe817c307b05290a2ea75ca3fd24c424e531
NishuDissanayake <63721945+NishuDissanayake@users.noreply.github.com>
2020-10-11 22:11:49 +0530

    Create salarycalc.c

---
+++ salarycalc.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int Basic,GrossRem,WorkInBonus,MonthlyBonus,sales,Years,AddAllow;
+    char workIn;
+
+                    printf ("Enter basic salary : ");
+                    scanf ("%d",&Basic);
+                    printf ("Enter years of service : ");
+                    scanf ("%d",&Years);
+                    printf ("Enter monthly sales : ");
+                    scanf ("%d",&sales);
+                    printf ("Enter 'L' if work in Lima, or N if not : ");
+                    scanf ("%s",&workIn);
+
+                    printf ("\n\n");
+                
+                
+               
+                
+
+    return 0;
+}
